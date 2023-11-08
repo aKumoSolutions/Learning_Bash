c8bb28ea8015dc22ff4dcd29e7b0e80da41612ec
Abdul Sharif <asharif@akumosolutions.io>
2023-11-08 00:42:58 +0000

    this is a test comment

--- Sept2023/useradd.sh
+++ Sept2023/useradd.sh
@@ -10,9 +10,8 @@ if [ $? == '0' ]; then
 elif [ $? == '9' ]; then 
     echo "The user $username already exists"
 
-elif [ $? == '3' ]; then 
-    echo "You entered a invalid username"
-
+# elif [ $? == '3' ]; then 
+#     echo "You entered a invalid username"
 else
     echo "There was a problem"
 fi
