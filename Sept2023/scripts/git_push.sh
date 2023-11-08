258592f08be42a74a201424c7528ae6aaa7133a1
Abdul Sharif <asharif@akumosolutions.io>
2023-11-08 02:08:31 +0000

    cleaning up repo

---
+++ Sept2023/scripts/git_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# -d .git 
+
+if [ -d .git ]; then
+    git add .
+    if [ $# -le 0 ]; then
+        read -p "Please enter commit message: " commit_message
+        git commit -m "$commit_message"
+    else
+        git commit -m "$1"
+    fi
+    git push
+else
+    echo "You are not in a repository 🫢"
+fi
+
+
+
