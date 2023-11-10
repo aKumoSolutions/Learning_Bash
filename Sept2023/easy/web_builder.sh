c2e7afd46b391cc3e497f505c9f2ed87d573b49e
Abdul Sharif <asharif@akumosolutions.io>
2023-11-10 21:03:04 +0000

    fix: adding the homework solutin

--- Sept2023/easy/web_builder.sh
+++ Sept2023/easy/web_builder.sh
@@ -6,20 +6,26 @@ fi
 
 file_path=$1
 
-# for existing_web in $(ls /var/www/html); do 
-# carvilla           carvilla  
-# edgecut            edgecut  
-# finexo             finexo  
-# little-fashion     little-fashion  
-# spering            spering
-#                    finter.zip
-
-
+    
 for web in $(cat $file_path); do 
     name="$(echo $web | awk -F "/" '{ print $9}' | awk -F "." '{ print $1 }')"
-    wget $web -P /tmp/
-    unzip /tmp/$name.zip -d /tmp/
-    rm -rf /tmp/*.zip
-    mkdir /var/www/html/$name
-    mv /tmp/*$name*/* /var/www/html/$name/
+    webs=$(ls /var/www/html)
+    while [[ $webs != *$name* ]]; do 
+        wget $web -P /tmp/
+        unzip /tmp/$name.zip -d /tmp/
+        rm -rf /tmp/*.zip
+        mkdir /var/www/html/$name
+        mv /tmp/*$name*/* /var/www/html/$name/
+        webs=$(ls /var/www/html)
+    done
 done
+
+
+# for web in $(cat $file_path); do 
+#     name="$(echo $web | awk -F "/" '{ print $9}' | awk -F "." '{ print $1 }')"
+#     wget $web -P /tmp/
+#     unzip /tmp/$name.zip -d /tmp/
+#     rm -rf /tmp/*.zip
+#     mkdir /var/www/html/$name
+#     mv /tmp/*$name*/* /var/www/html/$name/
+# done
